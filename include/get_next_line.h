6b8eb76901aca30beda17917d449a5e987bed30b
Bastien <bastien.rinck@epitech.eu>
2017-03-20 12:35:31 +0100

    oui

---
+++ include/get_next_line.h
@@ -0,0 +1,16 @@
+/*
+** get_next_line.h for get_nextline in /home/rectoria/delivery/Projets/Projet_getnextline
+** 
+** Made by Bastien
+** Login   <rectoria@epitech.net>
+** 
+** Started on  Mon Jan  2 11:09:48 2017 Bastien
+** Last update Thu Mar 16 09:56:19 2017 Bastien
+*/
+
+#ifndef GET_NEXT_LINE_H_
+# define GET_NEXT_LINE_H_
+
+char	*get_next_line(const int);
+
+#endif /* !GET_NEXT_LINE_H_ */
