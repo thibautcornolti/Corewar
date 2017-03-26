4685a64ef67622388023520beb03a89fc6a32329
Bastien <bastien.rinck@epitech.eu>
2017-03-26 16:55:56 +0200

    oui

--- label.c
+++ label.c
@@ -5,7 +5,7 @@
 ** Login   <rectoria@epitech.net>
 ** 
 ** Started on  Sun Mar 26 15:14:06 2017 Bastien
-** Last update Sun Mar 26 16:20:06 2017 Thibaut Cornolti
+** Last update Sun Mar 26 16:52:43 2017 Bastien
 */
 
 #include "my.h"
@@ -19,7 +19,6 @@ int	get_label_pos(char *name, t_label *label)
   temp = label;
   while (temp && my_strncmp(name, temp->lname, my_strlen(name)) && temp->next)
     temp = temp->next;
-  printf("%s\n", temp->lname);
   if (!my_strncmp(name, temp->lname, my_strlen(name)))
     return (temp->lpos);
   return (84);
@@ -30,7 +29,6 @@ int		label(char *fname, t_label **label)
   t_label	*temp;
   t_label	*new;
 
-  
   if ((new = malloc(sizeof(t_label))) == 0)
     return (84);
   my_memset(new, 0, sizeof(t_label));
@@ -42,6 +40,6 @@ int		label(char *fname, t_label **label)
   if (temp && temp->lname && !my_strncmp(fname, temp->lname, my_strlen(fname)))
     return (84);
   new->lname = fname;
-  temp->next = (*label == temp) ? NULL : new;
+  temp->next = (new == temp) ? NULL : new;
   return (0);
 }
