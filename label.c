e8d7a7cee09c4b5255f6b95acf69b6a320cae7c7
Bastien <bastien.rinck@epitech.eu>
2017-03-26 16:04:53 +0200

    oui

--- label.c
+++ label.c
@@ -5,7 +5,7 @@
 ** Login   <rectoria@epitech.net>
 ** 
 ** Started on  Sun Mar 26 15:14:06 2017 Bastien
-** Last update Sun Mar 26 16:01:27 2017 Thibaut Cornolti
+** Last update Sun Mar 26 16:04:42 2017 Bastien
 */
 
 #include "my.h"
@@ -29,16 +29,17 @@ int		label(char *fname, t_label **label)
   t_label	*temp;
   t_label	*new;
 
-  temp = *label;
+  
   if ((new = malloc(sizeof(t_label))) == 0)
     return (84);
   my_memset(new, 0, sizeof(t_label));
+  *label = (*label) ? *label : new;
+  temp = *label;
   while (temp && my_strncmp(fname, temp->lname, my_strlen(fname))
 	 && temp->next)
     temp = temp->next;
   if (temp && !my_strncmp(fname, temp->lname, my_strlen(fname)))
     return (84);
-  *label = (*label) ? *label : new;
   new->lname = fname;
   temp->next = (*label == temp) ? NULL : new;
   return (0);
