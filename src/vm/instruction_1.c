4b59b4e5aaff4974b5b627e2ff13145c1df3f8a5
Bastien <bastien.rinck@epitech.eu>
2017-04-01 18:48:03 +0200

    oui

--- src/vm/instruction_1.c
+++ src/vm/instruction_1.c
@@ -5,7 +5,7 @@
 ** Login   <luc.brulet@epitech.eu>
 ** 
 ** Started on  Thu Mar 30 16:35:29 2017 Luc
-** Last update Sat Apr  1 18:33:07 2017 Thibaut Cornolti
+** Last update Sat Apr  1 18:47:51 2017 Bastien
 */
 
 #include <unistd.h>
@@ -50,20 +50,18 @@ int	ld(t_inst *inst, t_ptr *ptr, t_map *map)
 
 int	st(t_inst *inst, t_ptr *ptr, t_map *map)
 {
-  if (inst->inst != 0x03 || !(inst->arg[0].type & T_REG) ||
-      inst->arg[1].type & T_DIR || (unsigned int)inst->arg[0].arg > REG_NUMBER)
+  if (inst->inst != 0x03 || !(inst->arg[0].type & T_REG) || inst->arg[1].type
+      & T_DIR || (unsigned int)inst->arg[0].arg > REG_NUMBER ||
+      (inst->arg[1].type & T_REG &&
+       (unsigned int)inst->arg[1].arg > REG_NUMBER))
     return (84);
-  if (inst->arg[1].type & T_REG && (unsigned int)inst->arg[1].arg <= REG_NUMBER)
+  if (inst->arg[1].type & T_REG)
     {
       ptr->father->reg[inst->arg[0].arg - 1] = inst->arg[1].arg;
       return (0);
     }
-  my_memncpy (map->arena + (ptr->index_map +
-			    inst->arg[1].arg % IDX_MOD) % MEM_SIZE,
-	      &ptr->father->reg[inst->arg[0].arg - 1], 4);
-  my_memncpy (map->color + (ptr->index_map +
-			    inst->arg[1].arg % IDX_MOD) % MEM_SIZE,
-	      &ptr->father->color, 4);
+  my_memncpy (map->arena + (ptr->index_map + inst->arg[1].arg % IDX_MOD) % MEM_SIZE, &ptr->father->reg[inst->arg[0].arg - 1], 4);
+  my_memncpy (map->color + (ptr->index_map + inst->arg[1].arg % IDX_MOD) % MEM_SIZE, &ptr->father->color, 4);
   return (0);
 }
 
