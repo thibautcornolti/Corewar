22fa842639efe9f943beb016416e7bf8e869edb6
Luc Brulet <luc.brulet@epitech.eu>
2017-04-01 15:48:05 +0200

    parsing nul a chier inshalla ca va segv a 300%

--- src/vm/parsing.c
+++ src/vm/parsing.c
@@ -5,7 +5,7 @@
 ** Login   <luc.brulet@epitech.eu>
 ** 
 ** Started on  Thu Mar 30 11:06:17 2017 Luc
-** Last update Thu Mar 30 19:06:43 2017 Thibaut Cornolti
+** Last update Sat Apr  1 15:47:13 2017 Luc
 */
 
 #include <stdlib.h>
@@ -38,21 +38,28 @@ static int	add_to_list(t_cmd **champ,
   return (0);
 }
 
-static int	get_info_champ(char **argv, int *i, int *adress, int *num)
+static int	get_info_champ(char **argv, int *i, int *adress, int *num, int var[3])
 {
   if (my_strcmp(argv[*i], "-n") == 0)
     {
       if ((my_str_isnum(argv[*i + 1])) == 0)
 	return (84);
-      *num = my_getnbr(argv[*i + 1]);
+      var[0] = my_getnbr(argv[(*i) + 1]);
       *i += 2;
     }
   if (my_strcmp(argv[*i], "-a") == 0)
     {
       if ((my_str_isnum(argv[*i + 1])) == 0)
 	return (84);
-      *adress = my_getnbr(argv[*i + 1]);
+      var[1] = my_getnbr(argv[*i + 1]);
       *i += 2;
+    }
+  if (my_strcmp(argv[*i], "-dump") == 0)
+    {
+      if ((my_str_isnum(argv[*i + 1])) == 0)
+	return (84);
+      var[2] = my_getnbr(argv[*i + 1]);
+      *i += 1;
     }
   return (0);
 }
@@ -61,24 +68,25 @@ int		pars_cmd(t_cmd **champ, char **argv)
 {
   int		i;
   char		*name;
-  int		num;
-  int		adress;
+  int		var[3];
 
-  if ((my_str_isnum(argv[1])) != 1)
-    return (84);
   i = 1;
-  num = 1;
-  adress = 0;
-  while (argv[++i] != NULL)
+  var[0] = 1;
+  var[1] = 0;
+  var[2] = 0;
+  while (argv[i] != NULL)
     {
-      if ((get_info_champ(argv, &i, &adress, &num)) == 84)
+      if ((get_info_champ(argv, &i, &adress, &num, var)) == 84)
 	return (84);
-      if (my_strcmp(argv[i], "-a") == 1 && my_strcmp(argv[i], "-n") == 1)
+      if ((my_strcmp(argv[i], "-a") == 1) && (my_strcmp(argv[i], "-n") == 1)
+	  && my_str_isnum(argv[i]) == 0)
       	{
 	  name = argv[i];
-      	  add_to_list(champ, num, adress, name);
-	  num += 1;
+      	  add_to_list(champ, var[0], var[1], name);
+	  var[0] += 1;
+	  var[1] = 0;
       	}
+      i++;
     }
   return (0);
 }
