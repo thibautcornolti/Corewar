d9a4baa0448d9989ee2a60719e09d3cae6be1c43
Thibaut Cornolti <thibaut.cornolti@epitech.eu>
2017-04-01 19:19:34 +0200

    oui

--- src/vm/instruction_4.c
+++ src/vm/instruction_4.c
@@ -5,7 +5,7 @@
 ** Login   <luc.brulet@epitech.eu>
 ** 
 ** Started on  Thu Mar 30 17:14:26 2017 Luc
-** Last update Sat Apr  1 17:49:28 2017 Thibaut Cornolti
+** Last update Sat Apr  1 19:05:05 2017 Thibaut Cornolti
 */
 
 #include <unistd.h>
@@ -15,11 +15,12 @@ int	aff(t_inst *inst, t_ptr *ptr, t_map *map)
 {
   char	c;
 
-  if (inst->inst != 0x10 || !(inst->arg[0].type & T_REG) ||
-      (unsigned int)inst->arg[0].arg >= REG_NUMBER)
-    return (84);
+  /* if (inst->inst != 0x10 || !(inst->arg[0].type & T_REG) || */
+  /*     (unsigned int)inst->arg[0].arg >= REG_NUMBER) */
+  /*   return (84); */
   (void) map;
-  c = ptr->father->reg[inst->arg[0].arg - 1] % 256;
+  //c = ptr->father->reg[inst->arg[0].arg - 1] % 256;
+  c = get_arg_value(&(inst->arg[0]), ptr, map);
   printf("aff:%x\n", c);
   write(1, &c, 1);
   write(1, "\n", 1);
